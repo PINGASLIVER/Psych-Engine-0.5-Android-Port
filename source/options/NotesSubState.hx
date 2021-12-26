6351fe49e29897fbce362ec2c3b9e1dec74b3b5c
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-26 17:44:19 +0200

    will no longer crash the game

--- source/options/NotesSubState.hx
+++ source/options/NotesSubState.hx
@@ -179,7 +179,7 @@ class NotesSubState extends MusicBeatSubstate
 
 		if (controls.BACK || (changingNote && controls.ACCEPT)) {
 			if(!changingNote) {
-				MusicBeatState.resetState();
+				close();//will no longer crash the game
 			} else {
 				changeSelection();
 			}
