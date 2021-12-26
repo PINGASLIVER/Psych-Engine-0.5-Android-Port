4475c5da027cca3fb614abea82268bf29c5994f6
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-26 14:30:43 +0200

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -91,19 +91,7 @@ class Main extends Sprite
 		#end
 
                 #if android
-                AndroidTools.requestPermission(Permissions.READ_EXTERNAL_STORAGE);
-                AndroidTools.requestPermission(Permissions.WRITE_EXTERNAL_STORAGE);
-
-                if (!FileSystem.exists(Main.getDataPath() + "assets"))
-                {
-                    Application.current.window.alert("Try copying assets/assets from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
-                    System.exit(0);//Will close the game
-                }
-                else if (!FileSystem.exists(Main.getDataPath() + "mods"))
-                {
-                    Application.current.window.alert("Try copying assets/mods from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
-                    System.exit(0);//Will close the game
-                }
+                Data.init();
                 #end
 
 		ClientPrefs.loadDefaultKeys();
