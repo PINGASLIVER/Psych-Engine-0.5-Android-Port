16b6426efd4d7924697825c1f7f8bff268d126be
Saw <77043862+jigsaw-4277821@users.noreply.github.com>
2021-12-26 14:28:20 +0200

    Good Thing

---
+++ source/Data.hx
@@ -0,0 +1,62 @@
+package;
+
+import sys.FileSystem;
+import lime.app.Application;
+import lime.system.System;
+import android.*;
+
+class Data
+{
+	
+	private static var dataPath1:String = null;
+	private static var dataPath2:String = null;
+	
+    function getPath1():String 
+    {
+        if (dataPath1 != null && dataPath1.length > 0) 
+        {
+            return dataPath1;
+        } 
+        else 
+        {
+            dataPath1 = "/storage/emulated/0/Android/data/" + Application.current.meta.get("packageName");
+        }
+        return dataPath1;
+    }
+    
+    function getPath2():String 
+    {
+        if (dataPath2 != null && dataPath2.length > 0) 
+        {
+            return dataPath2;
+        } 
+        else 
+        {
+            dataPath2 = "/storage/emulated/0/Android/data/";
+        }
+        return dataPath2;
+    }    
+
+	public static function init():Void
+	{
+        AndroidTools.requestPermission(Permissions.READ_EXTERNAL_STORAGE);
+        AndroidTools.requestPermission(Permissions.WRITE_EXTERNAL_STORAGE);
+
+        if (!FileSystem.exists(getPath2 + Application.current.meta.get("packageName")))
+            FileSystem.createDirectory(getPath2 + Application.current.meta.get("packageName");        
+
+        if (!FileSystem.exists(getPath1 + "/files/"))
+            FileSystem.createDirectory(getPath1 + "/files/");
+
+        if (!FileSystem.exists(Main.getDataPath() + "assets"))
+        {
+            Application.current.window.alert("Try copying assets/assets from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
+            System.exit(0);//Will close the game
+        }
+        else if (!FileSystem.exists(Main.getDataPath() + "mods"))
+        {
+            Application.current.window.alert("Try copying assets/mods from apk to " + " /storage/emulated/0/Android/data/" + Application.current.meta.get("packageName") + "/files/" + "\n" + "Press Ok To Close The App", "Check Directory Error");
+            System.exit(0);//Will close the game
+        }
+	}
+}
